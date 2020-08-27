4816088cd440a78ac6c0f43a956d6c6db502ad9a
icyftl <savap0@yandex.ru>
2020-08-28 02:01:19 +0300

    Docker support implemented

---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM python:3
+EXPOSE 8001
+
+RUN mkdir /opt/app
+WORKDIR /opt/app
+
+COPY requirements.txt .
+COPY source/ ./source/
+COPY CBot.py .
+COPY config.json .
+COPY core.py .
+
+RUN pip3 install -r requirements.txt
--- source/abstract/Event.py
+++ source/abstract/Event.py
@@ -3,16 +3,27 @@ from aiogram.utils.markdown import markdown_decoration
 
 class Event:
     def __init__(self, name: str, link: str, start_date: str, finish_date: str, format: str, weight: str, id):
-        # TODO: Refactor
-        # https://ru.stackoverflow.com/questions/1154855/python3-%d0%bf%d1%80%d0%be%d0%b3%d0%bd%d0%b0%d1%82%d1%8c-%d0%b2%d1%81%d0%b5-%d1%81%d0%b2%d0%be%d0%b9%d1%81%d1%82%d0%b2%d0%b0-%d0%ba%d0%bb%d0%b0%d1%81%d1%81%d0%b0-%d1%87%d0%b5%d1%80%d0%b5%d0%b7-%d0%bc%d0%b5%d1%82%d0%be%d0%b4
-        # map(func, (getattr(self, x) for x in dir(self)))
-        self.name = markdown_decoration.quote(name)
+        self.name = name
         self.link = link
-        self.start_date = markdown_decoration.quote(start_date)
-        self.finish_date = markdown_decoration.quote(finish_date)
-        self.format = markdown_decoration.quote(format)
-        self.weight = markdown_decoration.quote(weight)
+        self.start_date = start_date
+        self.finish_date = finish_date
+        self.format = format
+        self.weight = weight
         self.id = id.replace('https://ctftime.org/event/', '')
+        self.markdown_beautify()
+
+    def markdown_beautify(self):
+        for x in filter(lambda el: not el.startswith('__') and
+                                   not callable(getattr(self, el) and
+                                   el not in ['link', 'id']),
+                        dir(self)
+                        ):
+            print([x for x in filter(lambda el: not el.startswith('__') and
+                                   not callable(getattr(self, el) and
+                                   el not in ['link', 'id']),
+                        dir(self))])
+            print(getattr(self, x))
+            setattr(self, x, markdown_decoration.quote(getattr(self, x)))
 
     def __iter__(self):
         yield 'name', self.name
@@ -25,3 +36,5 @@ class Event:
 
     def __repr__(self):
         return f'<Event name={self.name}>'
+
+Event("lol","kek","n1ce","aaa","bbb.*","ф","-")
